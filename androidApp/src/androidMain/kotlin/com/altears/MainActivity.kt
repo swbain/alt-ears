945819a7ce10c748a34aec8cdfcbb7ec347143c7
Stephen Bain <swbain@gmail.com>
2026-02-05 02:03:58 +0000

    Force dark system bars (light icons) for dark theme

--- androidApp/src/androidMain/kotlin/com/altears/MainActivity.kt
+++ androidApp/src/androidMain/kotlin/com/altears/MainActivity.kt
@@ -2,6 +2,7 @@ package com.altears
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.altears.ui.App
@@ -9,7 +10,11 @@ import com.altears.ui.App
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        // Force dark status bar (light icons) since we're dark theme only
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(android.graphics.Color.TRANSPARENT),
+            navigationBarStyle = SystemBarStyle.dark(android.graphics.Color.TRANSPARENT)
+        )
         setContent {
             App()
         }
